2cc78c4ee9d2b216789855cf1d362bf8b93a57da
md jahidul islam <za0162hid@gmail.com>
2021-01-18 20:39:47 +0600

    env added

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+
+
 
 
 const port = 7000
@@ -19,7 +22,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-const uri = "mongodb+srv://volunteer:volunteer123@cluster0.7adfu.mongodb.net/volunteerNetwork?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7adfu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true});
 client.connect(err => {
   const collection = client.db("volunteerNetwork").collection("events");
